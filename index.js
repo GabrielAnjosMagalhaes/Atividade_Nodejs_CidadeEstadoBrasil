ddf954f48ce613d71a5f3d4e547cb7dffb94a702
Gabriel dos Anjos <95793375+GabrielAnjosMagalhaes@users.noreply.github.com>
2022-06-14 18:00:33 -0300

    Questão 1 e 2

--- index.js
+++ index.js
@@ -1,16 +1,17 @@
 fs = require("fs").promises
 
 //executando função de inicio
-inicio()
+//inicio()
+contadorMG()
 
 //função criada para executar outras funções ao rodar o código
 async function inicio(){ // criando função inicio de forma assícrona
  criarArquivos() //ao iniciar executar a função criarArquivos
-
-}
+ 
+}   
 
 async function criarArquivos(){ //essa função criará os arquivos que armazenarão os estados e as cidades importadas 
-    let dados = await fs.readFile("./importado/Estados.json") // recebendo valores do arquivo exportado: Estados.json
+    let dados = await fs.readFile("./importado/Estados.json") // recebendo dados do arquivo exportado: Estados.json
 
     //console.log(dados) //verificando 
 
@@ -30,3 +31,12 @@ async function criarArquivos(){ //essa função criará os arquivos que armazena
     }
 }
 
+async function contadorMG(uf){//função para retornar a quantidade de cidades baseado no estado selecionado (como por exemplo MG)
+//aqui irei repetir de certo modo o comando utilizado na função criarArquivos 
+const dados = await fs.readFile(`./estados/MG.json`) //lendo dados da pasta criada: "estados"
+const cidades = JSON.parse(dados) //convertendo dados para forma legível
+console.log(`Minas Gerais tem ${cidades.length} cidades`) // imprimindo o numero de cidades encontradas em MG.json
+return cidades.length // retornando o numero de cidades
+}
+
+    
